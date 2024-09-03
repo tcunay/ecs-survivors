d02870d3373b77b829c39bf9f6858c45a86aef55
Gevorg <gulkanyangk@mail.ru>
2024-09-04 00:16:08 +0300

    Finish Entity View Logic

--- src/ecs-survivors/Assets/Code/Infrastructure/View/EntityBehaviour.cs
+++ src/ecs-survivors/Assets/Code/Infrastructure/View/EntityBehaviour.cs
@@ -1,13 +1,22 @@
+using Code.Gameplay.Common.Collisions;
 using Code.Infrastructure.View.Registrars;
 using UnityEngine;
+using Zenject;
 
 namespace Code.Infrastructure.View
 {
     public class EntityBehaviour : MonoBehaviour, IEntityView
     {
         private GameEntity _entity;
+        private ICollisionRegistry _collisionRegistry;
 
         public GameEntity Entity => _entity;
+
+        [Inject]
+        private void Construct(ICollisionRegistry collisionRegistry)
+        {
+            _collisionRegistry = collisionRegistry;
+        }
 
         public void SetEntity(GameEntity entity)
         {
@@ -15,23 +24,41 @@ namespace Code.Infrastructure.View
             _entity.AddView(this);
             _entity.Retain(this);
 
-            foreach (IEntityComponentRegistrar registrar in GetComponentsInChildren<IEntityComponentRegistrar>())
-            {
-                registrar.RegisterComponents();
-            }
+            RegisterComponents();
+            RegisterColliders();
         }
 
         public void ReleaseEntity()
         {
-            foreach (IEntityComponentRegistrar registrar in GetComponentsInChildren<IEntityComponentRegistrar>())
-            {
-                registrar.UnRegisterComponents();
-            }
+            UnRegisterComponents();
+            UnregisterColliders();
             
             _entity.Release(this);
             _entity = null;
         }
         
-        
+        private void RegisterColliders()
+        {
+            foreach (Collider2D collider in GetComponentsInChildren<Collider2D>(includeInactive: true))
+                _collisionRegistry.Register(collider.GetInstanceID(), _entity);
+        }
+
+        private void UnregisterColliders()
+        {
+            foreach (Collider2D collider in GetComponentsInChildren<Collider2D>(includeInactive: true))
+                _collisionRegistry.Unregister(collider.GetInstanceID());
+        }
+
+        private void RegisterComponents()
+        {
+            foreach (IEntityComponentRegistrar registrar in GetComponentsInChildren<IEntityComponentRegistrar>())
+                registrar.RegisterComponents();
+        }
+
+        private void UnRegisterComponents()
+        {
+            foreach (IEntityComponentRegistrar registrar in GetComponentsInChildren<IEntityComponentRegistrar>())
+                registrar.UnRegisterComponents();
+        }
     }
 }
